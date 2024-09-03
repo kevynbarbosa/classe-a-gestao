6dab43ef8d99f515d46458b4d4bfa6779289e08c
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2024-09-03 00:27:15 -0300

    Ajustes de layout para menu com drawer em mobiles

--- resources/js/Layouts/Layout2.vue
+++ resources/js/Layouts/Layout2.vue
@@ -2,33 +2,34 @@
     <div class="flex min-h-screen bg-[#181822] text-slate-200">
         <!-- Header -->
         <div
-            class="fixed top-0 right-0 z-10 bg-blue-300 transition-header"
-            :class="{ 'w-header': sideBarVisible, 'w-full': !sideBarVisible }"
+            :class="['fixed top-0 right-0 z-10 bg-blue-300 transition-header', sideBarVisible ? 'w-header' : 'w-full']"
         >
             <div class="h-header p-4 pr-8 flex justify-between items-center">
                 <HeaderContent @toggleSideBar="toggleSideBar" @toggleRightBar="toggleRightBar" />
             </div>
         </div>
 
-        <!-- Header Mobile -->
-        <Drawer v-model:visible="rightBarVisible" header="Notificações" position="right">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat.
-            </p>
-        </Drawer>
+        <!-- Left menuMenu -->
+        <div class="hidden lg:block">
+            <Transition>
+                <div class="w-sidebar" v-if="sideBarVisible"></div>
+            </Transition>
 
-        <!-- Menu -->
-        <Transition>
-            <div class="w-sidebar" v-if="sideBarVisible"></div>
-        </Transition>
+            <Transition>
+                <div v-show="sideBarVisible" class="fixed h-full w-sidebar border-r border-[#2c2d33] overflow-y-auto">
+                    <LeftBarContent />
+                </div>
+            </Transition>
 
-        <Transition>
-            <div v-show="sideBarVisible" class="fixed h-full w-sidebar border-r border-[#2c2d33] overflow-y-auto">
-                <LeftBarContent />
-            </div>
-        </Transition>
+            <!-- Left menu Mobile -->
+            <Drawer v-if="windowWidth < 1024" v-model:visible="sideBarVisible" header="Menu" position="right">
+                <template #container="{ closeCallback }">
+                    <div class="bg-surface">
+                        <LeftBarContent />
+                    </div>
+                </template>
+            </Drawer>
+        </div>
 
         <!-- Right Drawer -->
         <Drawer v-model:visible="rightBarVisible" header="Notificações" position="right">
@@ -59,10 +60,23 @@ function toggleSideBar() {
     sideBarVisible.value = !sideBarVisible.value;
 }
 
-const rightBarVisible = ref(true);
+const rightBarVisible = ref(false);
 function toggleRightBar() {
     rightBarVisible.value = !rightBarVisible.value;
 }
+
+const windowWidth = ref(window.innerWidth);
+const updateWidth = () => {
+    windowWidth.value = window.innerWidth;
+};
+
+onMounted(() => {
+    window.addEventListener("resize", updateWidth);
+});
+
+onUnmounted(() => {
+    window.removeEventListener("resize", updateWidth);
+});
 </script>
 
 <style>
@@ -74,7 +88,13 @@ function toggleRightBar() {
 
 <style scoped>
 .w-header {
-    width: calc(100% - var(--sidebar-width));
+    width: 100%;
+}
+
+@media (min-width: 1024px) {
+    .w-header {
+        width: calc(100% - var(--sidebar-width));
+    }
 }
 
 .h-header {
