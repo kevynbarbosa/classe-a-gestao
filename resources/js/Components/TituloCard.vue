0686e06a18183315c6a1ab68a47eb4e85d177cbd
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2024-09-21 16:51:09 -0300

    Ajuste alinhamento das colunas do dataTable

---
+++ resources/js/Components/TituloCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mb-4">
+        <h3>{{ titulo }}</h3>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    titulo: {
+        type: String,
+        required: true,
+    },
+});
+const variable = ref(null);
+</script>
--- resources/js/Pages/Artista/Index.vue
+++ resources/js/Pages/Artista/Index.vue
@@ -1,15 +1,68 @@
 <template>
     <Head title="Artistas" />
 
-    <div class="surface-card">
-        <DataTable :value="artistas" tableStyle="min-width: 50rem">
-            <Column field="nome" header="Nome"></Column>
+    <Menu ref="menu" id="overlay_menu" :model="menuOpcoes" :popup="true" @hide="eventoSelecionado = null" />
+
+    <div class="card">
+        <TituloCard titulo="Artistas">
+            <Button label="Novo artista" icon="mdi mdi-plus"></Button>
+        </TituloCard>
+
+        <DataTable :value="artistas" paginator :rows="5" :rowsPerPageOptions="[2, 5, 10, 20, 50]">
+            <Column field="artista" header="Artista" sortable>
+                <template #body="{ data }">
+                    <Avatar label="KB" shape="circle" />
+                    {{ data.nome }}
+                </template>
+            </Column>
+
+            <Column field="actions" header="" class="column-right">
+                <template #body="{ data }">
+                    <Button
+                        icon="mdi mdi-dots-vertical"
+                        size="small"
+                        severity="secondary"
+                        aria-haspopup="true"
+                        aria-controls="overlay_menu"
+                        @click="abrirMenu($event, data)"
+                    />
+                </template>
+            </Column>
         </DataTable>
     </div>
 </template>
 
 <script setup>
+import TituloCard from "@/Components/TituloCard.vue";
 import { Head } from "@inertiajs/vue3";
 
 defineProps({ artistas: Array });
+
+// Seção Menu
+const menu = ref();
+const eventoSelecionado = ref();
+const menuOpcoes = ref([
+    {
+        label: "Ações",
+        items: [
+            {
+                label: "Editar",
+                icon: "pi pi-file-edit",
+            },
+
+            {
+                label: "Anexar documento",
+                icon: "pi pi-paperclip",
+                command: () => {
+                    abrirAnexarDocumentoDialog();
+                },
+            },
+        ],
+    },
+]);
+
+function abrirMenu(event, data) {
+    menu.value.toggle(event);
+    eventoSelecionado.value = data;
+}
 </script>
