2f7bd9077832e9642055ed4d79bc070276016992
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2024-10-11 00:45:51 -0300

    Ajuste dateUtils

--- resources/js/Utils/dateUtils.js
+++ resources/js/Utils/dateUtils.js
@@ -1,14 +1,20 @@
-function dateLocale(date, fuso = "-3") {
-    const [ano, mes, dia] = date.split("-").map(Number);
-    const dateObj = new Date(ano, mes - 1, dia);
-    return dateObj.toLocaleDateString();
-}
-
+// Parse a date string to Date object
 function parse2Date(date) {
+    if (!date) {
+        return null;
+    }
+
     const [ano, mes, dia] = date.split("-").map(Number);
     const dateObj = new Date(ano, mes - 1, dia);
 
     return dateObj;
+}
+
+// Formata a data formato americano para brasileiro
+function dateLocale(date, fuso = "-3") {
+    const [ano, mes, dia] = date.split("-").map(Number);
+    const dateObj = new Date(ano, mes - 1, dia);
+    return dateObj.toLocaleDateString();
 }
 
 export { dateLocale, parse2Date };
