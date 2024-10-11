4b1f488e8a2052f2f1390720c84b31cb346a2a05
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2024-10-11 13:53:25 -0300

    Locale pt-br

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,17 +21,17 @@ const CustomTheme = definePreset(Aura, {
     // Your customizations, see the following sections for examples
     semantic: {
         primary: {
-            50: "{violet.50}",
-            100: "{violet.100}",
-            200: "{violet.200}",
-            300: "{violet.300}",
-            400: "{violet.400}",
-            500: "{violet.500}",
-            600: "{violet.600}",
-            700: "{violet.700}",
-            800: "{violet.800}",
-            900: "{violet.900}",
-            950: "{violet.950}",
+            50: "{blue.50}",
+            100: "{blue.100}",
+            200: "{blue.200}",
+            300: "{blue.300}",
+            400: "{blue.400}",
+            500: "{blue.500}",
+            600: "{blue.600}",
+            700: "{blue.700}",
+            800: "{blue.800}",
+            900: "{blue.900}",
+            950: "{blue.950}",
         },
     },
 });
