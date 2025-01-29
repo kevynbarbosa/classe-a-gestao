4e44248df42fed40fa117e274c2eec2f55cdcd1e
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-01-29 17:13:35 -0300

    Alterando download para utilizar o axios

--- resources/js/Pages/DocumentoInterno/DownloadLoteDocumentos.vue
+++ resources/js/Pages/DocumentoInterno/DownloadLoteDocumentos.vue
@@ -27,7 +27,7 @@
 
             <div class="mt-4 flex justify-end gap-2">
                 <Button label="Cancelar" severity="secondary" @click="close" />
-                <Button label="Download" type="submit" :loading="form.processing" />
+                <Button label="Download" type="submit" :loading="downloading" />
             </div>
         </form>
     </Modal>
@@ -38,6 +38,7 @@ import TituloCard from "@/Components/TituloCard.vue";
 import { dateLocale } from "@/Utils/dateUtils";
 import { Head, useForm } from "@inertiajs/vue3";
 import { Modal } from "@inertiaui/modal-vue";
+import axios from "axios";
 
 const props = defineProps({
     evento: Object,
@@ -50,7 +51,38 @@ const form = useForm({
     documento_ids: props.documentos?.map((doc) => doc.id) ?? [],
 });
 
-function downloadLoteDocumentos() {
+function _downloadLoteDocumentos() {
     form.get(route("documentos-internos.lote-download"));
 }
+
+const downloading = ref(false);
+const downloadLoteDocumentos = async () => {
+    try {
+        downloading.value = true;
+        const response = await axios.get(route("documentos-internos.lote-download"), {
+            params: {
+                documento_ids: form.documento_ids,
+            },
+            responseType: "blob", // Importante para downloads
+        });
+
+        // Criar URL para o Blob
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+
+        // Criar um link temporário e disparar o download
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "arquivos.zip"); // Define o nome do arquivo
+        document.body.appendChild(link);
+        link.click();
+
+        // Remover o link após o download
+        link.remove();
+        window.URL.revokeObjectURL(url);
+    } catch (error) {
+        console.error("Erro ao baixar o arquivo:", error);
+    } finally {
+        downloading.value = false;
+    }
+};
 </script>
