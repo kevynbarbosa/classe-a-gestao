3e74c5827002ab305502de3d99556db382906f4d
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-02-25 14:49:28 -0300

    Instaurando o alwaysTo no email do HML

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'staging') { // Verifica se está em homologação
+            Mail::alwaysTo('kevynbarbosa1@gmail.com');
+        }
     }
 }
