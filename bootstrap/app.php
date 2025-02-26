8414b5d86423b8468ac32852cd29c3af2134161e
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-02-26 16:02:25 -0300

    Alterando comportamente de erros

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Inertia\Inertia;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,18 +21,23 @@ return Application::configure(basePath: dirname(__DIR__))
 
         //
     })
-    // ->withExceptions(function (Exceptions $exceptions) {
-    //     // InertiaJS page expired error
-    //     $exceptions->respond(function (Response $response) {
-    //         if ($response->getStatusCode() === 419) {
-    //             return back()->with([
-    //                 'message' => 'The page expired, please try again.',
-    //             ]);
-    //         }
+    // https://inertiajs.com/error-handling
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+            if (! app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+                return Inertia::render('Error', ['status' => $response->getStatusCode()])
+                    ->toResponse($request)
+                    ->setStatusCode($response->getStatusCode());
+            } elseif ($response->getStatusCode() === 419) {
+                return back()->with([
+                    'message' => 'The page expired, please try again.',
+                ]);
+            }
 
-    //         return $response;
-    //     });
-    // })
+            return $response;
+        });
+    })
+
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
