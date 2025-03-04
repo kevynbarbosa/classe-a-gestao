b95fdf80e1f4c1c65d686fcfc9c398759f1bb917
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-03-04 16:21:34 -0300

    Alterando texto

--- resources/views/mails/proposta_contratante.blade.php
+++ resources/views/mails/proposta_contratante.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <h1>Contratação para o evento {{ $evento->titulo }}</h1>
 <p>Olá, {{ $evento->contratante->nome_completo }}.</p>
-<p>Segue proposta de contratação para o evento com {{ $evento->artista->nome }}, a ser realizado em {{ $evento->data_hora->format('d/m/Y H:i') }} no local <b>{{ $evento->recinto }}</b>.</p>
+<p>Segue em anexo proposta de contratação para o evento com {{ $evento->artista->nome }}, a ser realizado em {{ $evento->data_hora->format('d/m/Y H:i') }} no local <b>{{ $evento->recinto }}</b>.</p>
 <p>Atenciosamente,<br>{{ config('app.name') }}</p>
 @endsection
 
