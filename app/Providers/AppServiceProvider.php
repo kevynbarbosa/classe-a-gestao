be0678b3655e53a4ad43a3b196f7b5f138a89bd8
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-03-05 01:25:23 -0300

    Alterando o email do alwaysTo para a Dri

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.env') === 'staging') { // Verifica se está em homologação
-            Mail::alwaysTo('kevynbarbosa1@gmail.com');
+            Mail::alwaysTo('tiraosi@yahoo.com.br');
         }
     }
 }
