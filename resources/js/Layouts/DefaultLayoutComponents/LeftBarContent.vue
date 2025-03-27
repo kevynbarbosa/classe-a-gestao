3bafbc52c7c72a3b9e66b3a2b39e0042b0bb09bb
Kevyn Barbosa <kevynbarbosa1@gmail.com>
2025-03-27 15:41:35 -0300

    Add modal link for form access and implement link copying functionality in LeftBarContent

--- resources/js/Layouts/DefaultLayoutComponents/LeftBarContent.vue
+++ resources/js/Layouts/DefaultLayoutComponents/LeftBarContent.vue
@@ -3,6 +3,12 @@
         <!-- Logo -->
         <ApplicationLogo class="h-12" />
 
+        <div class="my-4 flex justify-center">
+            <ModalLink href="#link-formulario">
+                <Button label="Link p/ contratantes" icon="mdi mdi-link-variant" />
+            </ModalLink>
+        </div>
+
         <!-- Menu -->
         <NavTitle label="Menu">
             <NavLink
@@ -78,6 +84,25 @@
                 </NavSubmenu>
             </NavSubmenu> -->
         </NavTitle>
+
+        <Modal name="link-formulario" max-width="sm">
+            <TituloCard titulo="Link do formulário"></TituloCard>
+
+            <div class="rounded bg-primary/20 p-2 text-center hover:bg-primary/40">
+                <a class="underline" :href="route('evento-workflow.contratante-formulario-aberto')" target="_blank">
+                    {{ route("evento-workflow.contratante-formulario-aberto") }}
+                </a>
+            </div>
+
+            <div class="w-full text-center">
+                <Button
+                    class="mt-2"
+                    label="Copiar link do formulário"
+                    icon="mdi mdi-content-copy"
+                    @click="copiarLink"
+                />
+            </div>
+        </Modal>
     </div>
 </template>
 
@@ -85,9 +110,36 @@
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 import NavLink from "@/Components/LeftMenu/NavLink.vue";
 import NavTitle from "@/Components/LeftMenu/NavTitle.vue";
+import TituloCard from "@/Components/TituloCard.vue";
+import { Modal, ModalLink } from "@inertiaui/modal-vue";
+import { useToast } from "primevue/usetoast";
 import { onMounted } from "vue";
 
 const props = defineProps([]);
+
+const toast = useToast();
+function copiarLink() {
+    const link = route("evento-workflow.contratante-formulario-aberto");
+    navigator.clipboard
+        .writeText(link)
+        .then(() => {
+            toast.add({
+                severity: "success",
+                summary: "Link copiado",
+                detail: "Link copiado para a área de transferência",
+                life: 3000,
+            });
+        })
+        .catch((err) => {
+            console.error("Erro ao copiar link: ", err);
+            toast.add({
+                severity: "error",
+                summary: "Erro",
+                detail: "Ocorreu um erro ao copiar o link",
+                life: 3000,
+            });
+        });
+}
 
 onMounted(() => {
     // Mounted
